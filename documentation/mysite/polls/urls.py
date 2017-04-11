8a0a3f078ec3083d07e6873f151e33b7bc9eea6e
taosilly <912472071@qq.com>
2017-04-11 20:23:34 +0800

    part 3, views template

--- documentation/mysite/polls/urls.py
+++ documentation/mysite/polls/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
+	#ex: /polls/
 	url(R'^$', views.index, name='index'),
+	#ex: /polls/5/
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	#ex: /polls/5/result/
+	url(R'^(?P<question_id>[0-9]+)/result$', views.result, name='result'),
+	#ex: /polls/5/vote/
+	url(R'^(?P<question_id>[0-9]+)/vote$', views.vote, name='vote')
 ]
