14e41d2bb14809f4defe6a54f21ab50d3d4a84f1
taosilly <912472071@qq.com>
2017-04-12 18:47:55 +0800

    part 7, admin page

--- documentation/mysite/polls/models.py
+++ documentation/mysite/polls/models.py
@@ -17,6 +17,9 @@ class Question(models.Model):
         now = timezone.now()
         return  now >= self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
